b989b43e3a52c220a518a93c02bd9239fa190fac
Krishna Chaitanya <chaitanya118@gmail.com>
2026-02-14 21:22:01 +0530

    provider node changes

--- services/core-services/src/main/java/com/onetattva/infron/core/controllers/NodesController.java
+++ services/core-services/src/main/java/com/onetattva/infron/core/controllers/NodesController.java
@@ -3,10 +3,13 @@ package com.onetattva.infron.core.controllers;
 import com.onetattva.infron.api.NodesApi;
 import com.onetattva.infron.api.model.*;
 import com.onetattva.infron.api.model.Node;
+import com.onetattva.infron.api.model.NodeCreate;
 import com.onetattva.infron.api.model.NodeList;
 import com.onetattva.infron.api.model.NodeProviderType;
+import com.onetattva.infron.api.model.NodeUpdate;
 import com.onetattva.infron.core.services.NodesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,16 +22,34 @@ public class NodesController implements NodesApi {
     private NodesService nodesService;
 
     @Override
-    public ResponseEntity<Node> getNode(UUID nodeId) {
+    public ResponseEntity<Node> getNode(UUID providerId, UUID nodeId) {
         Node node = nodesService.getNode(nodeId);
         return ResponseEntity.ok(node);
     }
 
     @Override
-    public ResponseEntity<NodeList> listNodes(Integer page, Integer perPage, String sort, UUID clusterId,
-                                              UUID datacenterId, String name, NodeProviderType providerType) {
-        NodeList nodeList = nodesService.listNodes(page, perPage, sort, clusterId, datacenterId, name,
+    public ResponseEntity<NodeList> listNodes(UUID providerId, Integer page, Integer perPage, String sort,
+                                              UUID clusterId, String name, NodeProviderType providerType) {
+        NodeList nodeList = nodesService.listNodes(page, perPage, sort, clusterId, null, name,
                 providerType);
         return ResponseEntity.ok(nodeList);
+    }
+
+    @Override
+    public ResponseEntity<Node> createNode(UUID providerId, NodeCreate nodeCreate) {
+        Node node = nodesService.createNode(providerId, nodeCreate);
+        return new ResponseEntity<>(node, HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<Node> updateNode(UUID providerId, UUID nodeId, NodeUpdate nodeUpdate) {
+        Node node = nodesService.updateNode(nodeId, nodeUpdate);
+        return ResponseEntity.ok(node);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteNode(UUID providerId, UUID nodeId) {
+        nodesService.deleteNode(nodeId);
+        return ResponseEntity.noContent().build();
     }
 }
